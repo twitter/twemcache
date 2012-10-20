b97399bed835c2db6c460950c6e5285c4c25d663
soarpenguin <soarpenguin@gmail.com>
2012-10-20 10:09:26 +0800

    fix macro definition strXcmp error for big endian

--- src/mc_ascii.c
+++ src/mc_ascii.c
@@ -159,32 +159,32 @@ static struct bound ntoken_bound[] = {
 
 #else
 
-#define str4cmp(m, c0, c1, c2, c3)                                          \
-    (m[0] == c0 && m[1] == c1 && m[2] == c2 && m[3] == c3)
+#define str4cmp(m, c0, c1, c2, c3) \
+(m[0] == c0 && m[1] == c1 && m[2] == c2 && m[3] == c3)
 
-#define str5cmp(m, c0, c1, c2, c3, c4)                                      \
-    (str4cmp(m, c0, c1, c2, c3) && (m[4] == c4))
+#define str5cmp(m, c0, c1, c2, c3, c4) \
+(str4cmp(m, c0, c1, c2, c3) && (m[4] == c4))
 
-#define str6cmp(m, c0, c1, c2, c3, c4, c5)                                  \
-    (str5cmp(m, c0, c1, c2, c3) && m[5] == c5)
+#define str6cmp(m, c0, c1, c2, c3, c4, c5) \
+(str5cmp(m, c0, c1, c2, c3, c4) && m[5] == c5)
 
-#define str7cmp(m, c0, c1, c2, c3, c4, c5, c6)                              \
-    (str6cmp(m, c0, c1, c2, c3) && m[6] == c6)
+#define str7cmp(m, c0, c1, c2, c3, c4, c5, c6) \
+(str6cmp(m, c0, c1, c2, c3, c4, c5) && m[6] == c6)
 
-#define str8cmp(m, c0, c1, c2, c3, c4, c5, c6, c7)                          \
-    (str7cmp(m, c0, c1, c2, c3) && m[7] == c7)
+#define str8cmp(m, c0, c1, c2, c3, c4, c5, c6, c7) \
+(str7cmp(m, c0, c1, c2, c3, c4, c5, c6) && m[7] == c7)
 
-#define str9cmp(m, c0, c1, c2, c3, c4, c5, c6, c7, c8)                      \
-    (str8cmp(m, c0, c1, c2, c3) && m[8] == c8)
+#define str9cmp(m, c0, c1, c2, c3, c4, c5, c6, c7, c8) \
+(str8cmp(m, c0, c1, c2, c3, c4, c5, c6, c7) && m[8] == c8)
 
-#define str10cmp(m, c0, c1, c2, c3, c4, c5, c6, c7, c8, c9)                 \
-    (str9cmp(m, c0, c1, c2, c3) && m[9] == c9)
+#define str10cmp(m, c0, c1, c2, c3, c4, c5, c6, c7, c8, c9) \
+(str9cmp(m, c0, c1, c2, c3, c4, c5, c6, c7, c8) && m[9] == c9)
 
-#define str11cmp(m, c0, c1, c2, c3, c4, c5, c6, c7, c8, c9, c10)            \
-    (str10cmp(m, c0, c1, c2, c3) && m[10] == c10)
+#define str11cmp(m, c0, c1, c2, c3, c4, c5, c6, c7, c8, c9, c10) \
+(str10cmp(m, c0, c1, c2, c3, c4, c5, c6, c7, c8, c9) && m[10] == c10)
 
-#define str12cmp(m, c0, c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11)       \
-    (str10cmp(m, c0, c1, c2, c3) && m[11] == c11)
+#define str12cmp(m, c0, c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11) \
+(str11cmp(m, c0, c1, c2, c3, c4, c5, c6, c7, c8, c9, c10) && m[11] == c11)
 
 #endif
 
